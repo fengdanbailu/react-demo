3ee5a9f4395762aa1fc5737224496bc6094c8c9d
guoming chen <chenguoming2015@163.com>
2021-03-31 21:43:20 +0800

    查看两段代码的效果

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
  * @Author: gm.chen
  * @Date: 2020-09-14 20:23:14
  * @LastEditors: gm.chen
- * @LastEditTime: 2021-03-31 00:23:38
+ * @LastEditTime: 2021-03-31 21:37:37
  * @Description: file content
  * @FilePath: /react-demo/src/App.js
  */
 import React from 'react'
 // import { Router, Route, Link } from 'react-router'
-import { BrowserRouter as Router, Route, NavLink,HashRouter } from 'react-router-dom';
+import {  NavLink,HashRouter,Route } from 'react-router-dom';
 import TScrollView from './exercise/tScrollView';
 import TPizzaTranslator from './exercise/tPizzaTranslator'
 import THome from './exercise/tHome'
+import TStyle from './exercise/tStyle'
+import TFlexBox from './exercise/tFlexbox'
   
 
 function App() {
@@ -22,10 +24,14 @@ function App() {
           <li><NavLink to='/home' activeClassName="active">home</NavLink></li>
           <li><NavLink to='/scrollView' activeClassName="active">scrollView</NavLink></li>
           <li><NavLink to='/pizzaTranslator' activeClassName="active">pizzaTranslator</NavLink></li>
+          <li><NavLink to='/style' activeClassName="active">style</NavLink></li>
+          <li><NavLink to='/flexbox' activeClassName="active">flexbox</NavLink></li>
         </ul>
-        <Route exact path='/home' component={THome} /> 
+        <Route exact path='/home' component={THome} ></Route> 
         <Route path='/scrollView' component={TScrollView}></Route>
         <Route path='/pizzaTranslator' component={TPizzaTranslator}></Route>
+        <Route path='/style' component={TStyle}></Route>
+        <Route path='/flexbox' component={TFlexBox}></Route>
       </div>
     </HashRouter>
   )
